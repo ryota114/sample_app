5429e18385e87e46b8e40d2c98862cc985b7e46e
EC2 Default User <ec2-user@ip-172-31-35-123.ec2.internal>
2021-03-03 14:48:02 +0000

    Initial Commit

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,32 @@
+class TodolistsController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    # List.newで空のリストを生成する?
+    @list = List.new
+  end
+
+  def create
+    # 1.データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 2.データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 3.トップ画面へリダイレクト
+    # redirect_to "/top"
+    #詳細画面へリダイレクト
+    redirect_to todolist_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private #privateから下はコントローラーの中でしか呼び出せない。よってプログラムの下に書く
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+end
